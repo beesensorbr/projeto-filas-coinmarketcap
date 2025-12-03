00720419c5d49a0cac5ce8e9a2fbdae0d503b8ed
BEESENSOR <beesensorbr@gmail.com>
2025-12-02 22:38:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown(
 
     /* Banner com imagem grande do Bitcoin */
    .btc-banner {
-    position: relative;
+    position: static;
     width: 40%;
     height: 260px;
     border-radius: 18px;
@@ -660,6 +660,7 @@ with aba_upload:
                 )
     else:
         st.info("Envie um arquivo CSV para habilitar as análises desta aba.")
+
 
 
 
